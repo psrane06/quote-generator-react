bcdd4b2698035e168051b3ecdda0b59130dd23d1
Pankaj Rane <pankaj.rane@whitehatjr.com>
2024-03-15 19:07:17 +0530

    getting author specific all quotes, fixed CSS and components for the same

--- src/App.js
+++ src/App.js
@@ -9,9 +9,16 @@ function App() {
   const [quote, setQuote] = useState("");
   const [quoteAuthor, setQuoteAuthor] = useState("");
   const [quoteGenre, setQuoteGenre] = useState("");
+  const [allQuotes, setAllQuotes] = useState([]);
+  const [view, setView] = useState("random");
 
+
+  /**
+   * Fetches a random quote from API
+   */
   const fetchQuote = async () => {
     try {
+      setView('random');
       const response = await fetch("https://quote-garden.onrender.com/api/v3/quotes/random");
       const data = await response.json();
       setQuote(data.data[0].quoteText);
@@ -22,20 +29,24 @@ function App() {
     }
   };
 
-  /**l
-   * 
-   * 
+  /**
+   * Gets all the quotes of a particular author
    */
 
-  const fetchAllAuthorQuotes = async (author) => {
+  const fetchAllAuthorQuotes = async () => {
     try {
-
+      setView('author');
+      const response = await fetch(`https://quote-garden.onrender.com/api/v3/quotes?author=${quoteAuthor}`);
+      const quoteData = await response.json();
+      setAllQuotes(quoteData.data.map(data => { return data.quoteText }));
     } catch (e) {
       console.log(e);
     }
   }
 
-
+  /**
+   * Used to get quote from API on initial load
+   */
   useEffect(() => {
     fetchQuote();
   }, []);
@@ -43,12 +54,28 @@ function App() {
   return (
     <div className="App">
       <Header fetchQuote={fetchQuote} />
-      <Quote quote={quote} />
-      <ForwardButton quoteAuthor={quoteAuthor} quoteGenre={quoteGenre} onClick={fetchAllAuthorQuotes} />
+      {view === 'random' ?
+        <>
+          <Quote quote={quote} />
+          <ForwardButton quoteAuthor={quoteAuthor} quoteGenre={quoteGenre} onClick={fetchAllAuthorQuotes} />
+        </>
+        :
+        <>
+          <div className='all_quotes_wrapper'>
+            <h1>{quoteAuthor}</h1>
+            {allQuotes.map((quotes, index) => { return <Quote key={index} quote={quotes} /> })}
+          </div>
+        </>
+      }
     </div>
   );
 }
 
+/**
+ * 
+ * @param fetchQuote function calls function  API to get a random quote 
+ * @returns 
+ */
 function Header({ fetchQuote }) {
   return (
     <header className="App-header">
@@ -62,25 +89,23 @@ function Header({ fetchQuote }) {
 
 function Quote(props) {
   return (
-    <main>
-      <div>
-        <p className='quote'>
-          {props.quote}
-        </p>
-      </div>
-    </main >
+    <div className='quote_wrapper'>
+      <p className='quote'>
+        {props.quote}
+      </p>
+    </div >
   );
 }
 
 function ForwardButton(props) {
   return (
-    <div className='author_wrapper'>
+    <div className='author_wrapper' onClick={props.onClick}>
       <div className='author'>
         <div>
           <p className='quote_author'>{props.quoteAuthor}</p>
           <p className='quote_genre'>{props.quoteGenre}</p>
         </div>
-        <div className='forward_button' onClick={props.onClick}>
+        <div className='forward_button'>
           <span className="material-symbols-outlined">arrow_right_alt</span>
         </div>
       </div>
