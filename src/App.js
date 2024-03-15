404d2baac9b4085762949def16559c8bbd5d5b43
Pankaj Rane <pankaj.rane@whitehatjr.com>
2024-03-15 16:49:53 +0530

    seperated components, fixed initial load issue with useEffect hook,

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+//Learn Typescript
+//react router for multiple pages
+//array of objects
+
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [quote, setQuote] = useState("");
@@ -7,31 +11,53 @@ function App() {
   const [quoteGenre, setQuoteGenre] = useState("");
 
   const fetchQuote = async () => {
-    const response = await fetch("https://quote-garden.onrender.com/api/v3/quotes/random");
-    const data = await response.json();
-    setQuote(data.data[0].quoteText);
-    setQuoteAuthor(data.data[0].quoteAuthor);
-    setQuoteGenre(data.data[0].quoteGenre);
+    try {
+      const response = await fetch("https://quote-garden.onrender.com/api/v3/quotes/random");
+      const data = await response.json();
+      setQuote(data.data[0].quoteText);
+      setQuoteAuthor(data.data[0].quoteAuthor);
+      setQuoteGenre(data.data[0].quoteGenre);
+    } catch (e) {
+      console.log(e);
+    }
   };
-  // fetchQuote();
+
+  /**l
+   * 
+   * 
+   */
+
+  const fetchAllAuthorQuotes = async (author) => {
+    try {
+
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
 
   return (
     <div className="App">
-      <Header />
-      <Quote quote={quote} quoteAuthor={quoteAuthor} quoteGenre={quoteGenre} />
+      <Header fetchQuote={fetchQuote} />
+      <Quote quote={quote} />
+      <ForwardButton quoteAuthor={quoteAuthor} quoteGenre={quoteGenre} onClick={fetchAllAuthorQuotes} />
     </div>
   );
+}
 
-  function Header() {
-    return (
-      <header className="App-header">
-        <button onClick={fetchQuote}>
-          <span>random</span>
-          <span class="material-symbols-outlined"> autorenew </span>
-        </button>
-      </header>
-    );
-  }
+function Header({ fetchQuote }) {
+  return (
+    <header className="App-header">
+      <button onClick={fetchQuote}>
+        <span>random</span>
+        <span className="material-symbols-outlined"> autorenew </span>
+      </button>
+    </header>
+  );
 }
 
 function Quote(props) {
@@ -42,16 +68,23 @@ function Quote(props) {
           {props.quote}
         </p>
       </div>
+    </main >
+  );
+}
+
+function ForwardButton(props) {
+  return (
+    <div className='author_wrapper'>
       <div className='author'>
         <div>
           <p className='quote_author'>{props.quoteAuthor}</p>
           <p className='quote_genre'>{props.quoteGenre}</p>
         </div>
-        <div className='forward_button'>
-          <span class="material-symbols-outlined">arrow_right_alt</span>
+        <div className='forward_button' onClick={props.onClick}>
+          <span className="material-symbols-outlined">arrow_right_alt</span>
         </div>
       </div>
-    </main >
+    </div>
   );
 }
 
